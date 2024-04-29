19a02f5d37129a9451d6a754644af82a67b04244
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-04-29 13:10:54 +0530

    classes and objects: getter and setter

--- app/src/main/java/com/ani/kotlindemos/cls-obj.kt
+++ app/src/main/java/com/ani/kotlindemos/cls-obj.kt
@@ -3,31 +3,30 @@ package com.ani.kotlindemos
 class Config
 
 class DbConfig( // primary
-    val url :String,
-    val port: Int
-)
+    private var url :String,
+    var port: Int
+) {
+    var dbUrl: String
+        get() = this.url
+        set(value) { this.url = value  }
+}
 
 class NwConfig {
-
-    var a: Int = 0
-
+    private var a: Int = 0
     constructor(a : Int = 0) { // secondary
         this.a = a
     }
-
     fun connect() = println("Connected")
-
     fun disconnect(): Boolean = false
-
 }
-
 
 fun main() {
 
     val cfg: Config = Config()
 
     val dbCfg = DbConfig(url = "abc", port = 1234)
-    println(dbCfg.url)
+    println(dbCfg.dbUrl) // getter
+    dbCfg.dbUrl = "localhost"
     println(dbCfg.port)
 
     val nwCfg = NwConfig(67)
