4ef3c94dc6accbbf8f0a8b9c047e9dc41f23ad82
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-04-30 15:43:50 +0530

    linear and relative layout

---
+++ AndroidIntro/app/src/main/java/com/ani/androidintro/LayoutDemoAvtivity.kt
@@ -0,0 +1,15 @@
+package com.ani.androidintro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LayoutDemoAvtivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_layout_demo_avtivity)
+    }
+}
