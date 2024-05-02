1505758356e53cac579e604c14ceee3fd01eb817
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-05-02 15:47:43 +0530

    Spring CRUD App : POST call

--- crud-app/src/main/kotlin/com/ani/rest/crud/SimpleController.kt
+++ crud-app/src/main/kotlin/com/ani/rest/crud/SimpleController.kt
@@ -3,6 +3,8 @@ package com.ani.rest.crud
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,10 +13,25 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class SimpleController {
 
+    private val app : App = App(id = "00000", cnt = 0)
+
     @GetMapping
-    public fun show(): ResponseEntity<App> {
-        val app = App(id = "12", cnt = 90)
+    fun show(): ResponseEntity<App> {
         println("-------> Client requested this method")
         return  ResponseEntity.ok(app)
     }
+
+    @PostMapping
+    fun create(@RequestBody app: App) : ResponseEntity<String> {
+
+        val (id, cnt) = app
+//        this.app.id = app.id
+//        this.app.cnt = app.cnt
+
+        this.app.id = id
+        this.app.cnt =cnt
+
+        return ResponseEntity.ok(id)
+    }
+
 }
