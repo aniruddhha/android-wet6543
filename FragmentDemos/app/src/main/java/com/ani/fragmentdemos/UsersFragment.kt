f4b1ec5c99d8c510ba862a9ddaa1190ac5f5027b
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-05-06 15:09:16 +0530

    removing item from recycler view

--- FragmentDemos/app/src/main/java/com/ani/fragmentdemos/UsersFragment.kt
+++ FragmentDemos/app/src/main/java/com/ani/fragmentdemos/UsersFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class UsersFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +17,34 @@ class UsersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_users, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val users = listOf(
+            User(1, "abc"),
+            User(2, "pqr"),
+            User(3, "lmn"),
+            User(4, "xyz"),
+            User(5, "tuv"),
+            User(6, "ddr"),
+            User(7, "tty"),
+            User(8, "qwe"),
+            User(9, "iop"),
+            User(10, "but"),
+        )
+
+        val adapter = UserAdapter(users)
+        adapter.setOnDeleteListener {
+            Toast.makeText(requireActivity(), "$it", Toast.LENGTH_LONG).show()
+
+        }
+
+        val recUsrs =  view.findViewById<RecyclerView>(R.id.recUsr)
+        recUsrs.layoutManager = LinearLayoutManager(requireActivity())
+        recUsrs.addItemDecoration(DividerItemDecoration(requireActivity(), DividerItemDecoration.VERTICAL))
+
+        recUsrs.adapter = adapter
     }
 }
