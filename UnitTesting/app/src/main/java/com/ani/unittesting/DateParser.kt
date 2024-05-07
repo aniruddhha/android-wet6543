c83c94ac55e3da7b43ecc275866b0534dcfb2b91
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-05-07 12:12:24 +0530

    unit testing : date parsing

--- UnitTesting/app/src/main/java/com/ani/unittesting/DateParser.kt
+++ UnitTesting/app/src/main/java/com/ani/unittesting/DateParser.kt
@@ -1,15 +1,21 @@
 package com.ani.unittesting
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Locale
 
 class DateParser(
-    private val dt: LocalDate
+    private val dt: Date
 ) {
-
 
     // the date object should be considered and function should return date
     // in 01-01-2020
-    fun parseDate() : String {
-        TODO("Complete The Code")
+//    @RequiresApi(Build.VERSION_CODES.O)
+    fun parseDate(format: String): String {
+        return SimpleDateFormat(format, Locale.getDefault()).format(dt)
     }
 }
--- UnitTesting/app/src/test/java/com/ani/unittesting/DateParserTest.kt
+++ UnitTesting/app/src/test/java/com/ani/unittesting/DateParserTest.kt
@@ -1,13 +1,23 @@
 package com.ani.unittesting
 
+import org.junit.Assert
 import org.junit.Test
 import java.time.LocalDate
+import java.util.Date
 
 class DateParserTest {
 
     @Test
     fun checkWorking() {
-        val dp = DateParser(LocalDate.now())
-        val str = dp.parseDate()
+        val dp = DateParser(Date())
+        val str = dp.parseDate("dd-MM-yyyy")
+        Assert.assertEquals("07-05-2024", str)
+    }
+
+    @Test
+    fun checkWrongFormat() {
+        val dp = DateParser(Date())
+        val str = dp.parseDate("dd-MM-yyyy")
+        Assert.assertEquals("2024-05-07", str)
     }
 }
